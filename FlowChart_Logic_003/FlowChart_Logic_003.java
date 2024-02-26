27fe96535ba9945099b4f4a10b8a8c29219fb936
Balachander K <94047622+balachander-k@users.noreply.github.com>
2024-02-26 19:03:58 +0530

    26-02-2024

---
+++ FlowChart_Logic_003/FlowChart_Logic_003.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class FlowChart_Logic_003
+{
+    public static void main(String[] arg)
+    {
+        int x=obj.nextInt(),y=obj.nextInt();
+        while(x<=y)
+        {
+            System.out.print(x+" ");
+            x++;
+        }
+    }
+}
